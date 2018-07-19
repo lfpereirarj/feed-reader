2e0035230eada7fc59feb78ca0e34be00de3b666
Luiz Felipe <lf.pereirarj@gmail.com>
2018-07-19 12:31:54 -0300

    Adiciando mensagens de erros

--- backend/api/feed/feed.js
+++ backend/api/feed/feed.js
@@ -3,14 +3,14 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const postSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { type: String, required: [true, 'Informe o título do post.'] },
     text: { type: String, required: false },
     url: { type: String, required: true },
     read: { type: Boolean, default:false, required: false }
 })
 
 const feedSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: [true, 'Informe o nome do feed.'] },
     url: { type: String, required: true },
     posts: [postSchema]
 })
